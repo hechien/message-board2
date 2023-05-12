6987a466b9bca83cb575a273fc502e38a5621812
HeChien Hsu <hechien@me.com>
2023-05-13 01:47:23 +0800

    Fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '172.31.7.156', user: 'deploy', roles: %w{app db web}
+server 'ec2-35-78-116-73.ap-northeast-1.compute.amazonaws.com', user: 'apps', roles: %w{app db web}
 
 
 # role-based syntax
